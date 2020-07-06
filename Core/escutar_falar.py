3ecff2da0a395d204dc4336b91ec57fa495edcb1
RenatoRegg <renatoreggiani@bb.com.br>
2020-07-06 20:13:41 -0300

    Ajuste na criação de audios

--- Core/escutar_falar.py
+++ Core/escutar_falar.py
@@ -2,15 +2,17 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 from unicodedata import normalize
-
-
+import json
+import os
 ##
 def cria_audio(texto):
     """Cria o audio utilizado TTS do google translator"""
-    tts = gTTS(texto, lang='pt-br', lang_check=False)
-    nome_arq = normalize('NFKD', frase).encode('ASCII', 'ignore').decode('utf-8')
-    tts.save(f'audios/{nome_arq}.mp3')
-    print("Estou aprendendo o que você disse...")
+    texto = texto.replace('estagiário', '').strip()
+    nome_arq = normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8')  # Remove acentos das frases
+    if os.path.isfile(f'audios/{nome_arq}.mp3') == False:
+        print("Estou aprendendo o que você disse...")
+        tts = gTTS(texto, lang='pt-br', lang_check=False)
+        tts.save(f'audios/{nome_arq}.mp3')
     # Da play ao audio
     playsound(f'audios/{nome_arq}.mp3')
 
@@ -33,12 +35,20 @@ def ouvir_microfone():
         print("Não entendi")
 
 
+##
+if __name__ == '__main__':
+    #tarefas_conhecidas = json('/tarefas/dict_tarefas.json')
+    frase = ouvir_microfone()
+    cria_audio(frase)
+
+    print(frase)
+
+##
+#tarefas_conhecidas = json.load(open('tarefas/dict_tarefas.json'))
+##
+#tarefas_conhecidas['nova'] = 'no]va'
+##
 
 
 ##
-frase = ouvir_microfone()
-cria_audio(frase)
 
-print(frase)
-
-##
