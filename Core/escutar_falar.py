b43f82b477490e459d7509be00b3aba02d63811e
RenatoRegg <renatoreggiani@bb.com.br>
2020-07-07 08:24:23 -0300

    ajustes comandos

--- Core/escutar_falar.py
+++ Core/escutar_falar.py
@@ -1,15 +1,14 @@
+import json
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 from unicodedata import normalize
-import json
 import os
 
 
 ##
 def cria_audio(texto):
     """Cria o audio utilizado TTS do google translator"""
-    texto = texto.replace('estagiário', '').strip()
     nome_arq = normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8')  # Remove acentos das frases
     if not os.path.isfile(f'audios/{nome_arq}.mp3'):
         print("Estou aprendendo o que você disse...")
@@ -29,6 +28,7 @@ def ouvir_microfone():
     try:
         texto = microfone.recognize_google(audio, language='pt-BR')  # Transforma audio em texto
         if 'estagiário' in texto.lower():
+            texto = texto.replace('estagiário', '').strip()
             print("Você disse: " + texto)
             return texto
     # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
@@ -37,12 +37,14 @@ def ouvir_microfone():
 
 
 ##
+
 if __name__ == '__main__':
     dados = json.load(open('tarefas/dict_tarefas.json', encoding='utf8'))
     frase = ouvir_microfone()
     cria_audio(frase)
 
     print(frase)
+
 
 ##
 
